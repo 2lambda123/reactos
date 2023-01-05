bf884672598d96879dfe5ae52d85800cf2219ca3
Timo Kreuzer <timo.kreuzer@reactos.org>
2023-01-05 04:51:14 +0200

    reenable msvcrt file

--- modules/rostests/winetests/msvcrt/testlist.c
+++ modules/rostests/winetests/msvcrt/testlist.c
@@ -24,9 +24,7 @@ const struct test winetest_testlist[] =
     { "data", func_data },
     { "dir", func_dir },
     { "environ", func_environ },
-#ifndef _M_AMD64 // UM exception + hang
     { "file", func_file },
-#endif
     { "headers", func_headers },
     { "heap", func_heap },
     { "locale", func_locale },
