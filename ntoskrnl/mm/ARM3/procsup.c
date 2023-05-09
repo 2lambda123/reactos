2a14a9bad039e2f82797f341cd45fa9a717de5b2
Timo Kreuzer <timo.kreuzer@reactos.org>
2023-05-09 23:23:49 +0300

    FIXME: Erase Stack PTEs before releasing them

--- ntoskrnl/mm/ARM3/procsup.c
+++ ntoskrnl/mm/ARM3/procsup.c
@@ -237,6 +237,8 @@ MmDeleteKernelStack(IN PVOID StackBase,
             Pfn1 = MiGetPfnEntry(PageFrameNumber);
             if (Pfn1->u3.e1.PageLocation != ActiveAndValid) __debugbreak();
 
+            PointerPte->u.Long = 0;
+
             /* Now get the page of the page table mapping it */
             PageTableFrameNumber = Pfn1->u4.PteFrame;
             Pfn2 = MiGetPfnEntry(PageTableFrameNumber);
